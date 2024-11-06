93db28b1f529012339c75b7ec20166d9edcce965
jambrizz <ambrizjovani@gmail.com>
2024-11-06 00:29:37 -0800

    Updated nav bar

--- codex/app/ui/dashboard/nav-links.tsx
+++ codex/app/ui/dashboard/nav-links.tsx
@@ -2,18 +2,38 @@ import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
+  ChartBarSquareIcon,
+  BanknotesIcon,
 } from '@heroicons/react/24/outline';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+    {
+        name: 'Home',
+        href: '/dashboard',
+        icon: HomeIcon
+    },
+    {
+        name: 'Clients',
+        href: '/dashboard/customers',
+        icon: UserGroupIcon
+    },
+    {
+        name: 'Processes',
+        href: '/dashboard/invoices',
+        icon: DocumentDuplicateIcon,
+    },
+    {
+        name: 'Reports',
+        href: '/dashboard/reports',
+        icon: ChartBarSquareIcon,
+    },
+    {
+        name: 'Budget',
+        href: '/dashboard/budget',
+        icon: BanknotesIcon,
+    },
 ];
 
 export default function NavLinks() {
