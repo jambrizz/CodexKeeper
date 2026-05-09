c4ed9b462c52dca5df7ab87991170ff95ccd3785
jambrizz <ambrizjovani@gmail.com>
2026-05-09 16:16:39 -0700

    updates to the home page dashboard

--- codex/app/dashboard/page.tsx
+++ codex/app/dashboard/page.tsx
@@ -6,9 +6,39 @@ export const metadata: Metadata = {
     title: 'Home',
 };
 export default function Page() {
+    const contractsData = () => {
+        //Create a function to pull contracts and see the details of the contract
+        // async & fetch
+    }
+
+    const reportedDSS = () => {
+        // Create a function to show the cases that have been reported to DSS
+        // async & fetch
+    }
+
+    const unreportedDSS = () => {
+        // create a function to show all the DSS cases that have not been marked as reported
+        // async & fetch
+    }
+
+    const cases = () => {
+        // create a function to show the cases that are complete, pending signature, pending data entry
+        // async & fetch
+    }
+
+    const averageProcessing = () => {
+        // create a function to show the average data entry times for each process
+        // async & fetch
+    }
+
     return (
         <>
-            <h1 >Home</h1>
+            <div className="flex flex-col items-center">
+                <h1 className="text-2xl font-semibold text-gray-700" >Welcome to CodexKeeper</h1>
+            </div>
+            <div>
+
+            </div>
         </>
     );
         
